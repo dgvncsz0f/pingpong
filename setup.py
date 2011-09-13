00a204646ec2680c4e38a6065e92132e42799012
dsouza <dsouza@bitforest.org>
2011-09-13 18:05:30 -0300

    writing debian package and updating setup.py to include samples

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-setup( name         = "pingpong",
-       version      = "0.0.1",
-       packages     = find_packages(exclude=["tests", "tests.*"]),
-       author       = "Diego Souza",
-       author_email = "dsouza at bitforest.org",
-       description  = "A library for quickly creating line-based interactive programs",
-       keywords     = "interactive line-based protocols"
+VERSION = "0.0.1"
+
+setup( name             = "pingpong",
+       version          = VERSION,
+       packages         = find_packages(exclude=["tests", "tests.*"]),
+       author           = "Diego Souza",
+       author_email     = "dsouza+pingpong@bitforest.org",
+       description      = "A library for quickly creating line-based interactive programs",
+       keywords         = "interactive line-based protocols",
+       data_files       = ( "/etc/pingpong", [ "pingpong/samples/ping.tac",
+                                               "pingpong/samples/hpflex10.tac"
+                                             ]
+                          ),
+       install_requires = [ "twisted >= 11.0.0",
+                            "TwistedConch >= 11.0.0"
+                          ]
      )
